10fa01568e12148526be64491358efae3b83e09f
Kam Banwait <kam@scriptedpixels.co.uk>
2025-04-20 17:47:52 +0100

    Removes logout handling from index

--- pages/index.vue
+++ pages/index.vue
@@ -4,25 +4,11 @@ definePageMeta({
   middleware: ['auth'],
 })
 
-const { user, clear: clearSession } = useUserSession()
+const { user } = useUserSession()
 
-const logout = async () => {
-  await clearSession()
-  navigateTo('/login')
-}
 
 </script>
 
 <template>  
-  <main v-if="user">
-    <h1>Welcome {{ user.name }}</h1>
-    <UButton @click="logout">Logout</UButton>
-    <p>index</p>
-  </main>
-  <main v-else>
-    <NuxtLink to="/login">
-      Log in
-    </NuxtLink>
-  </main>
     </template>
 </template>
